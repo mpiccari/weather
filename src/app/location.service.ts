d408297e3f040ebe4b117f914e94e3646e9306f0
mirko.piccari <SOS3UFA@posteitaliane.it>
2023-11-13 14:33:53 +0100

    punto 1 fatto

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@angular/core';
-import {WeatherService} from "./weather.service";
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export const LOCATIONS : string = "locations";
 
 @Injectable()
 export class LocationService {
+  //locations has been removed and replaced with this BeahiourSubject
+  private _locations$ : BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
-  locations : string[] = [];
-
-  constructor(private weatherService : WeatherService) {
+  //WeatherService has been removed
+  constructor() {
     let locString = localStorage.getItem(LOCATIONS);
-    if (locString)
-      this.locations = JSON.parse(locString);
-    for (let loc of this.locations)
-      this.weatherService.addCurrentConditions(loc);
+    if (locString) {
+      this._locations$.next(JSON.parse(locString));
+    }
+  }
+
+  getLocationsAsObs(): Observable<string[]> {
+    return this._locations$.asObservable();
   }
 
   addLocation(zipcode : string) {
-    this.locations.push(zipcode);
-    localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-    this.weatherService.addCurrentConditions(zipcode);
+    let locationsList: string[] = this._locations$.value;
+    locationsList.push(zipcode);
+    localStorage.setItem(LOCATIONS, JSON.stringify(locationsList));
+    this._locations$.next(locationsList);
   }
 
   removeLocation(zipcode : string) {
-    let index = this.locations.indexOf(zipcode);
+    let locationsList: string[] = this._locations$.value;
+    let index: number = locationsList.indexOf(zipcode);
     if (index !== -1){
-      this.locations.splice(index, 1);
-      localStorage.setItem(LOCATIONS, JSON.stringify(this.locations));
-      this.weatherService.removeCurrentConditions(zipcode);
+      locationsList.splice(index, 1);
+      localStorage.setItem(LOCATIONS, JSON.stringify(locationsList));
+      this._locations$.next(locationsList);
     }
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,25 +1,49 @@
-import {Injectable, Signal, signal} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Injectable, OnDestroy, Signal, signal} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 
 import {HttpClient} from '@angular/common/http';
 import {CurrentConditions} from './current-conditions/current-conditions.type';
 import {ConditionsAndZip} from './conditions-and-zip.type';
 import {Forecast} from './forecasts-list/forecast.type';
+import { LocationService } from './location.service';
 
 @Injectable()
-export class WeatherService {
+export class WeatherService implements OnDestroy {
 
   static URL = 'http://api.openweathermap.org/data/2.5';
   static APPID = '5a4b2d457ecbef9eb2a71e480b947604';
   static ICON_URL = 'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
   private currentConditions = signal<ConditionsAndZip[]>([]);
-
-  constructor(private http: HttpClient) { }
+  locationsChangeSub: Subscription; //this subscription manages changes of locations
+  
+  constructor(private http: HttpClient, locationService: LocationService) {
+    this.locationsChangeSub = locationService.getLocationsAsObs().subscribe(
+      locations => {
+        //search locations not present in currentConditions and add it to currentConditions
+        locations.filter(location => !this.currentConditions().find(
+          condition => condition.zip == location)
+        ).forEach(newLocation => this.addCurrentConditions(newLocation));
+        
+        /*search zipcodes present in currentConditions and not present in location.
+          After I remove it from currentConditions
+        */
+        this.currentConditions().filter(condition => !locations.find(
+          location => location == condition.zip)
+        ).forEach(removedZip => this.removeCurrentConditions(removedZip.zip));
+      }
+    )
+  }
+  
+  ngOnDestroy(): void {
+    this.locationsChangeSub.unsubscribe();
+  }
 
   addCurrentConditions(zipcode: string): void {
     // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
     this.http.get<CurrentConditions>(`${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`)
-      .subscribe(data => this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data})));
+      .subscribe(data => {
+        this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data}))
+      });
   }
 
   removeCurrentConditions(zipcode: string) {
@@ -57,5 +81,4 @@ export class WeatherService {
     else
       return WeatherService.ICON_URL + "art_clear.png";
   }
-
 }
