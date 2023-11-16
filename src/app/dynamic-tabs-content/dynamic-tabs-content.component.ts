e6cf0c6a7b300e862f6877d707f589382bbb077f
mirko.piccari <SOS3UFA@posteitaliane.it>
2023-11-16 19:57:19 +0100

    customizzazione stile dei tabs

--- src/app/dynamic-tabs-content/dynamic-tabs-content.component.ts
+++ src/app/dynamic-tabs-content/dynamic-tabs-content.component.ts
@@ -13,18 +13,27 @@ export interface TabModel {
 
   export class DynamicTabsContentComponent {
     /* as inputs: Every tab in this component has a contents template associated.
-    Each template is represented in an ng-template sent to the component via 
-    content projection feature. templates input is a list of all this ng-template
-    ElementRef and their associated string keys. tabs input is a list of all tabs 
-    of the component. For each tab we have tab title, template object context 
-    (not mandatory) and string key associated with its ElementRef template. Finally
-    for a correct rendering of each tab in html component file we need of directive
-    ngTemplateOutlet that make use for each tab of his templateRef and his 
-    template object context (if pesent).*/
+      Each template is represented in an ng-template sent to the component via 
+      content projection feature. templates input is a list of all this ng-template
+      ElementRef and their associated string keys. tabs input is a list of all tabs 
+      of the component. For each tab we have tab title, template object context 
+      (not mandatory) and string key associated with its ElementRef template. Finally
+      for a correct rendering of each tab in html component file we need of directive
+      ngTemplateOutlet that make use for each tab of his templateRef and his 
+      template object context (if pesent). areRemovableTabs is a boolean that if set
+      to true allows the dynamic removal of tabs.
+      In the end we have a series of inputs to customize the display of the tabs and
+      an output (removeTabEmitter) for removing a specific tag
+    */
     
     @Input() tabs: TabModel[];
     @Input() areRemovableTabs: boolean;
     @Input() templates: {key: string, content: TemplateRef<any>}[];
+    @Input() bgColorTab: string;
+    @Input() bgColorSelectedTab: string;
+    @Input() textColorTab: string;
+    @Input() textColorSelectedTab: string;
+    
     @Output() removeTabEmitter: EventEmitter<{tab: TabModel, index: number}> = 
       new EventEmitter<{tab: TabModel, index: number}>();
     activeTab: number = 0;
