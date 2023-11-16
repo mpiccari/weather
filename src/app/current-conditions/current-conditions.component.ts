a0f920e4d6bdb485a41f15698b2d58d85865db4f
mirko.piccari <SOS3UFA@posteitaliane.it>
2023-11-16 19:26:41 +0100

    ok punto 2

--- src/app/current-conditions/current-conditions.component.ts
+++ src/app/current-conditions/current-conditions.component.ts
@@ -3,6 +3,7 @@ import {WeatherService} from "../weather.service";
 import {LocationService} from "../location.service";
 import {Router} from "@angular/router";
 import {ConditionsAndZip} from '../conditions-and-zip.type';
+import { TabModel } from 'app/dynamic-tabs-content/dynamic-tabs-content.component';
 
 @Component({
   selector: 'app-current-conditions',
@@ -10,7 +11,19 @@ import {ConditionsAndZip} from '../conditions-and-zip.type';
   styleUrls: ['./current-conditions.component.css']
 })
 export class CurrentConditionsComponent {
-
   private weatherService = inject(WeatherService);
+  private locationService = inject(LocationService);
   protected currentConditionsByZip: Signal<ConditionsAndZip[]> = this.weatherService.getCurrentConditions();
+
+  getTabsArray(): TabModel[] {
+    return this.currentConditionsByZip().map(el => ({
+      contextTmplObj: el,
+      templateKey: 'locationTmpl',
+      tabTitle: el.data.name + ' (' + el.zip + ')'
+    }));
+  }
+
+  removeTab(tabInfo: {tab: TabModel, index: number}) {
+    this.locationService.removeLocation((<ConditionsAndZip> tabInfo.tab.contextTmplObj).zip);
+  }
 }
--- src/app/dynamic-tabs-content/dynamic-tabs-content.component.ts
+++ src/app/dynamic-tabs-content/dynamic-tabs-content.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input, TemplateRef } from "@angular/core";
+import { Component, EventEmitter, Input, Output, TemplateRef } from "@angular/core";
 
+export interface TabModel {
+  contextTmplObj?: any;
+  templateKey: string;
+  tabTitle: string;
+}
 @Component({
     selector: 'app-dynamic-tabs-content',
     templateUrl: './dynamic-tabs-content.component.html',
@@ -7,13 +12,33 @@ import { Component, Input, TemplateRef } from "@angular/core";
   })
 
   export class DynamicTabsContentComponent {
-    @Input() tabs: {city: string, templateTagName: string}[];
-    @Input() templates: {key: string, content: TemplateRef<any>}[]
+    /* as inputs: Every tab in this component has a contents template associated.
+    Each template is represented in an ng-template sent to the component via 
+    content projection feature. templates input is a list of all this ng-template
+    ElementRef and their associated string keys. tabs input is a list of all tabs 
+    of the component. For each tab we have tab title, template object context 
+    (not mandatory) and string key associated with its ElementRef template. Finally
+    for a correct rendering of each tab in html component file we need of directive
+    ngTemplateOutlet that make use for each tab of his templateRef and his 
+    template object context (if pesent).*/
+    
+    @Input() tabs: TabModel[];
+    @Input() areRemovableTabs: boolean;
+    @Input() templates: {key: string, content: TemplateRef<any>}[];
+    @Output() removeTabEmitter: EventEmitter<{tab: TabModel, index: number}> = 
+      new EventEmitter<{tab: TabModel, index: number}>();
     activeTab: number = 0;
 
     getTemplateRef(indice : number): TemplateRef<any> {
       return this.templates.find(template => 
-        template.key == this.tabs[indice].templateTagName
+        template.key == this.tabs[indice].templateKey
       ).content;
+    }
+
+    removeTab(tab: TabModel, index: number) {
+      this.removeTabEmitter.emit({tab, index});
+      if(index == this.activeTab && this.activeTab == this.tabs.length - 1) {
+        this.activeTab = 0;
+      }
     }
   }
