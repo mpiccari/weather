d8d15c1605a29710839ee871d21cc4c83c93421a
mirko.piccari <SOS3UFA@posteitaliane.it>
2023-11-19 01:16:38 +0100

    avanzamenti

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,6 @@
 import {Injectable, OnDestroy, Signal, signal} from '@angular/core';
-import {Observable, Subscription} from 'rxjs';
-
+import {Observable, Subscription, of} from 'rxjs';
+import { tap } from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 import {CurrentConditions} from './current-conditions/current-conditions.type';
 import {ConditionsAndZip} from './conditions-and-zip.type';
@@ -10,8 +10,9 @@ import { LocationService } from './location.service';
 const LOCATIONS : string = "locations"
 const RESPONSES_CACHE_TIME : string = "responses_cache_time" //Utility for testing responses cache
 const ZIPS_CONDITION : string = "zips_condition";
-export const DEFAULT_RESPONSES_CACHE_TIME_IN_MILLISECONDS: number = 7200000; //2h = 60 x 60 x 2 x 1000 ms
-interface CacheZipDates {zip: string, dates: CurrentConditions, lastUpdate: number};
+const ZIPS_FORECAST : string = "zips_forecast";
+export const DEFAULT_RESPONSES_CACHE_TIME_IN_MILLISECONDS: number = 20000;//7200000; //2h = 60 x 60 x 2 x 1000 ms
+interface CacheZipDates {zip: string, dates: CurrentConditions | Forecast, lastUpdate: number};
 @Injectable()
 export class WeatherService implements OnDestroy {
 
@@ -50,16 +51,16 @@ export class WeatherService implements OnDestroy {
   }
 
   addCurrentConditions(zipcode: string): void {
-    // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
     if(!this.currentConditions().find(cond => cond.zip == zipcode)) {
       // Search if dates are present in browser's cache and in valid cache time interval from last update
       const savedZipConditions: CacheZipDates[] = <CacheZipDates[]> this.getArrayByStorage(ZIPS_CONDITION);
       const zipCondition: CacheZipDates | undefined = savedZipConditions.find(
         date => date.zip == zipcode && new Date().getTime() - date.lastUpdate <= this.responsesCacheTime);
       if(zipCondition) {
-        this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data: zipCondition.dates}));
+        this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data: <CurrentConditions> zipCondition.dates}));
       } else {
         //get dates by network
+        // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
         this.http.get<CurrentConditions>(`${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`)
         .subscribe(data => {
           this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data}))
@@ -68,6 +69,7 @@ export class WeatherService implements OnDestroy {
             zipsInStorage.push(zipcode);
             localStorage.setItem(LOCATIONS, JSON.stringify(zipsInStorage));
           }
+          //save response in browser's storage
           savedZipConditions.push({zip: zipcode, dates: data, lastUpdate: (new Date()).getTime()});
           localStorage.setItem(ZIPS_CONDITION, JSON.stringify(savedZipConditions));
         });
@@ -86,6 +88,8 @@ export class WeatherService implements OnDestroy {
     zipsInStorage.splice(zipsInStorage.findIndex(zip => zip == zipcode), 1);      
     localStorage.setItem(LOCATIONS, JSON.stringify(zipsInStorage));
     this.removeCacheResponsesByZip(ZIPS_CONDITION, zipcode);
+    this.removeCacheResponsesByZip(ZIPS_FORECAST, zipcode);
+    
   }
 
   getCurrentConditions(): Signal<ConditionsAndZip[]> {
@@ -93,8 +97,24 @@ export class WeatherService implements OnDestroy {
   }
 
   getForecast(zipcode: string): Observable<Forecast> {
-    // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
-    return this.http.get<Forecast>(`${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`);
+    // Search if dates are present in browser's cache and in valid cache time interval from last update
+    const savedZipForecast: CacheZipDates[] = <CacheZipDates[]> this.getArrayByStorage(ZIPS_FORECAST);
+    
+    const zipForecast: CacheZipDates | undefined = savedZipForecast.find(
+      date => date.zip == zipcode && new Date().getTime() - date.lastUpdate <= this.responsesCacheTime);
+    if(zipForecast) {
+      return of(<Forecast> zipForecast.dates)
+    } else {
+      //get dates by network
+      // Here we make a request to get the forecast data from the API. Note the use of backticks and an expression to insert the zipcode
+      return this.http.get<Forecast>(`${WeatherService.URL}/forecast/daily?zip=${zipcode},us&units=imperial&cnt=5&APPID=${WeatherService.APPID}`)
+        .pipe(tap((response: Forecast) => {
+          //save response in browser's storage
+          savedZipForecast.push({zip: zipcode, dates: response, lastUpdate: (new Date()).getTime()});
+          localStorage.setItem(ZIPS_FORECAST, JSON.stringify(savedZipForecast));
+        })
+      );
+    }
   }
 
   getWeatherIcon(id): string {
